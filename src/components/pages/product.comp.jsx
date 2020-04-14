fd41464c5fe9e88333965198519bbfaa6fd6bee5
vophuctaiVN <vofktai@gmail.com>
2020-04-14 15:43:35 +0700

    #UI_filters

--- src/components/pages/product.comp.jsx
+++ src/components/pages/product.comp.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import {ProductList} from '../atoms/ProductsList';
+import {ListBrand} from '../atoms/ListBrand';
 
 function Product() {
   return (
     <div>
-
+<ListBrand/>
 <ProductList/>
     </div>
   )
