677a72194c5774600eb350df33a78501820b11fd
Loi <loia5tqd001@gmail.com>
2020-05-09 20:02:20 +0700

    feat: add header [wip]

--- src/components/templates/header.comp.jsx
+++ src/components/templates/header.comp.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
-
 import routes from '../../utils/routes';
-import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectAmountProduct } from '../../redux/cart/selectors';
+
+import { HeaderContainer, Logo, Form, SearchInput, SearchButton, CartIcon } from './header.styled';
 
 function Header() {
+  const amountProducts = useSelector(selectAmountProduct);
 
   return (
-    <header>
-      <NavLink to={routes.product.path}>Go to Product Page</NavLink> <br/>
-      <NavLink to={routes.cart.path}>Go to Cart Page</NavLink> <br />
-      --- <br />
-    </header>
+    <HeaderContainer>
+      <Logo to={routes.home.path}>Phonee</Logo>
+
+      <Form>
+        <SearchInput
+          type="text"
+          placeholder="Bạn tìm gì..."
+          ariaLabel="Bạn tìm gì..."
+          maxLength={40}
+        />
+        <SearchButton>
+          <ion-icon name="search" />
+        </SearchButton>
+      </Form>
+
+      <CartIcon to={routes.cart.path}>
+        <ion-icon name="cart" />
+        <span>({amountProducts})</span>
+      </CartIcon>
+    </HeaderContainer>
   );
 }
 
--- src/components/templates/header.styled.js
+++ src/components/templates/header.styled.js
@@ -1,17 +1,58 @@
 import styled, { css } from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const HeaderContainer = styled.header`
-  ${(props) =>
-    props.isStuck &&
-    css`
-      position: sticky;
-      top: 0;
-      z-index: 999;
-      background: ${props.theme.styles.background};
-      animation: ${props.theme.animations.stickyHeader} 0.3s;
-      box-shadow: ${props.theme.styles.shadowDown1};
+  max-width: 120rem;
+  margin: 0 auto 2em;
+  padding: 0.2em 1.3em;
+  background: ${(props) => props.theme.colors.blueDarker2};
+  color: ${(props) => props.theme.colors.white};
+  display: flex;
+  align-items: center;
+`;
 
-      display: flex;
-      justify-content: space-between;
-    `}
+export const Logo = styled(Link)`
+  font-size: 2.5em;
+  margin-right: 1em;
+  display: inline-block;
+  color: ${(props) => props.theme.colors.white};
+  font-weight: ${(props) => props.theme.fw.bold};
+  text-shadow: 0 1px 2px #0003;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+  position: relative;
+`;
+
+export const SearchInput = styled.input`
+  padding: 0.6em 2em 0.6em 1em;
+  outline: none;
+  border-radius: 4px;
+  border: none;
+`;
+
+export const SearchButton = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
+  font-size: 1.2em;
+  color: ${(props) => props.theme.colors.greyLight3};
+  position: absolute;
+  top: 50%;
+  right: 0.5rem;
+  transform: translateY(-50%);
+  outline: none;
+`;
+
+export const CartIcon = styled(Link)`
+  margin-left: auto;
+  color: ${(props) => props.theme.colors.white};
+  ${(props) => props.theme.mixins.flexCenter}
+
+  ion-icon {
+    font-size: 1.5em;
+  }
 `;
