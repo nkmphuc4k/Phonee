0237234efadc0a31d9b7e72938a5b0acf4a69e97
Loi <loia5tqd001@gmail.com>
2020-05-11 17:58:46 +0700

    feat: add footer

--- src/components/templates/footer.comp.jsx
+++ src/components/templates/footer.comp.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import { FooterContainer, Container, PhoneNumber, CopyRight } from './footer.styled';
+
 function Footer() {
   return (
-    <footer>
-      <br /> --- <br />
-      This is footer
-    </footer>
+    <FooterContainer>
+      <Container>
+        Số điện thoại hỗ trợ: <PhoneNumber>1900.0000</PhoneNumber> <br />
+        <b>Địa chỉ:</b> KTX Khu B, phường Đông Hòa, thị xã Dĩ An, tỉnh Bình Dương <br />
+        <CopyRight href="https://github.com/loia5tqd001/Phonee">
+          <strong>Copyright by Nguyen Huynh Loi &copy; 2020</strong>
+        </CopyRight>
+      </Container>
+    </FooterContainer>
   );
 }
 
--- src/components/templates/footer.styled.js
+++ src/components/templates/footer.styled.js
@@ -1,7 +1,20 @@
 import styled from 'styled-components';
 
-export const FooterContainer = styled.footer`
-  padding: 3rem 2rem;
-  font-weight: ${(props) => props.theme.fw.light};
+export const FooterContainer = styled.footer``;
+
+export const Container = styled.div`
+  color: ${(props) => props.theme.colors.white};
+  background: ${(props) => props.theme.colors.blueDarker2};
+  padding: 1em 3em;
+  margin: 3em auto 0;
+  max-width: 120rem;
 `;
 
+export const PhoneNumber = styled.span`
+  font-weight: ${(props) => props.theme.fw.semiBold};
+  font-size: 1.2em;
+`;
+
+export const CopyRight = styled.a`
+  color: inherit;
+`;
